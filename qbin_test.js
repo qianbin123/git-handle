a2098d49cb2424232bb8e8e048ace7fa8152cf63
qianbin <1797661770@qq.com>
2020-06-09 22:33:32 +0800

    Revert "改远程仓库"
    This reverts commit aec40679d419a049b7bcce3f0a244c304a21eaf2.

--- qbin_test.js
+++ qbin_test.js
@@ -1,5 +1,6 @@
 console.log("第二次提交");
-console.log("我是从master上切出的branch_B分支，第一次提交，请多指教");
+console.log("我是从master上切出的branch_B分支，第resert次提交，请多指教");
 console.log("改远程仓库");
 console.log("改远程仓库成ssh传输");
 console.log("revert一下");
+
